fe81cfc4a550baa96f9c55b25aa1fda7db8771af
Timothy Kempf <the.kaempfer@gmail.com>
2017-04-02 15:27:32 -0700

    use regular ass callbacks

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const gpio = require('pi-gpio');
 
 const PIN = 16;
 
-gpio.open(PIN, 'output', (error) => {
+gpio.open(PIN, 'output', function (error) {
   if (error) {
     console.log('Error:', error);
   }
 
-  gpio.write(PIN, 1, () => {
-    setTimeout(() => {
+  gpio.write(PIN, 1, function () {
+    setTimeout(function () {
       gpio.close(PIN);
     }, 10 * 1000);
   });
